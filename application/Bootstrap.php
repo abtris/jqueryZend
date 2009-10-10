b73ace3e94d11870a18157768ca03765a620c24b
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2009-10-10 17:59:30 +0200

    Datapicker demo added

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,26 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+    protected function _initAutoload ()
+    {
+        $moduleLoader = new Zend_Application_Module_Autoloader(
+        array('namespace' => '' , 'basePath' => APPLICATION_PATH));
+        $moduleLoader->addResourceType('controller', 'controllers', 'Controller');
+        return $moduleLoader;
+    }
+
+
+    protected function _initView()
+    {
+
+        $view= new Zend_View();
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
+        $view->addHelperPath('ZendX/JQuery/View/Helper/', 'ZendX_JQuery_View_Helper');
+        $viewRenderer->setView($view);
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+    }
 
 }
 
