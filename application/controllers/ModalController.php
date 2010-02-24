e249a1f3d4fedd9eabe238699ab536b779faeb21
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-02-24 11:06:56 +0100

    Add modal controller

---
+++ application/controllers/ModalController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * @author prskavecl
+ * @version  * @package  * @filesource
+ */
+/**
+ * @package @@PACKAGE@@
+ */
+class ModalController extends Zend_Co
+{
+
+}
